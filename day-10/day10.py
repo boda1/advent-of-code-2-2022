06b449eaeea3b88bbfd3d6145c5d7a8dd336bf1d
Adam Boda <adam.boda@UK1022-F403TM.local>
2022-12-17 10:40:30 +0000

    Refactored day 10 removing ref to previous values

--- day-10/day10.py
+++ day-10/day10.py
@@ -1,44 +1,23 @@
-previous_cycles = 0
 cycles = 0
-previous_x = 0
 X = 1
 sum_of_signals = 0
 
 
-def calc_signals_prev(prev_x, current_cycles):
-    return prev_x * current_cycles
-
-
-def calc_signals(current_x, current_cycles):
-    return current_x * current_cycles
+def check_cycle():
+    if cycles in [20, 60, 100, 140, 180, 220]:
+        return X * cycles
+    return 0
 
 
 with open('input.txt') as f:
     for instruction in f:
+        cycles += 1
+        sum_of_signals += check_cycle()
         if instruction.startswith('addx'):
             no_to_add = int(instruction.split(' ')[1])
-
-            # Addition instructions: split line finding number to be added,
-            # increment number of cycles by 2, add number to X
-
-            previous_cycles = cycles
-            cycles += 2
-            previous_x = X
-            X += no_to_add
-        elif instruction.startswith('noop'):
-
-            # Do nothing other than increment number of cycles
-
-            previous_x = X
-            previous_cycles = cycles
             cycles += 1
-        cycles_since_instruction = cycles - previous_cycles
-
-        for c in [20, 60, 100, 140, 180, 220]:
-            if previous_cycles < c < cycles or cycles == c and cycles_since_instruction <= 2:
-                sum_of_signals += calc_signals_prev(previous_x, c)
-            elif cycles == c:
-                sum_of_signals += calc_signals(X, c)
+            sum_of_signals += check_cycle()
+            X += no_to_add
 
 
 print(sum_of_signals)
